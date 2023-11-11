cd5061e02e30d5a9557dc7615d74ac1c61d95c5a
RaihanEXE99 <brainsta@brainstation.com.bd>
2023-11-11 20:52:16 +0600

    db change agent

--- property/views.py
+++ property/views.py
@@ -5,8 +5,7 @@ from rest_framework.response import Response
 from django.forms.models import model_to_dict
 from .models import *
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny import math
-=======
+from rest_framework.permissions import AllowAny
 
 from .serializer import AddressSerializer, PropertyDetailsSerializer,PropertySerializer
 from rest_framework import generics
@@ -15,19 +14,7 @@ from rest_framework.response import Response
 from .models import Property, Address, PropertyDetails
 
 from rest_framework.views import APIView
-
-<<<<<<< HEAD
 import math
-=======
-
-from .serializer import AddressSerializer, PropertyDetailsSerializer,PropertySerializer
-from rest_framework import generics
-from rest_framework import status
-from rest_framework.response import Response
-from .models import Property, Address, PropertyDetails
-
-from rest_framework.views import APIView
->>>>>>> 79bfe526891b114ccd47ce69edfe4de5a8230aa2
 # Create your views here.
 
 
