49741bdb1601087a4606441f732239d24e8fd8ff
Sadman Sakib Jisan <65308095+JisanEAE77@users.noreply.github.com>
2023-11-12 04:08:14 +0600

    nkk

--- property/serializer.py
+++ property/serializer.py
@@ -12,7 +12,7 @@ class PropertyDetailsSerializer(serializers.ModelSerializer):
         model = PropertyDetails
         fields = '__all__'
 
-class PropertySerializer(serializers.ModelSerializer):
+# class PropertySerializer(serializers.ModelSerializer):
     address = AddressSerializer()
     details = PropertyDetailsSerializer()
 
@@ -30,7 +30,31 @@ class PropertySerializer(serializers.ModelSerializer):
 
             property = Property.objects.create(address=address, details=details, **validated_data)
             return property
+class PropertySerializer(serializers.ModelSerializer):
+    address = AddressSerializer()
+    details = PropertyDetailsSerializer()
+    
+    class Meta:
+        model = Property
+        fields = ['title', 'price', 'price_unit', 'price_type', 'thumbnail', 
+                  'property_category', 'property_status', 'post_type', 'loc', 'lat', 'long',
+                  'date', 'desc', 'address', 'details', 'hide_contact', 'user']
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            address_data = validated_data.pop('address')
+            details_data = validated_data.pop('details')
+            address = Address.objects.create(**address_data)
+            details = PropertyDetails.objects.create(**details_data)
+
+            property_instance = Property.objects.create(
+                address=address, 
+                details=details, 
+                **validated_data
+            )
+            return property_instance
         
+
 from .models import Image
 
 class ImageSerializer(serializers.ModelSerializer):
