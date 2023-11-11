8bb62ba5eeffcf234eed6a50227ec1ee17799b9b
Sadman Sakib Jisan <65308095+JisanEAE77@users.noreply.github.com>
2023-11-11 20:44:09 +0600

    Update views.py

--- property/views.py
+++ property/views.py
@@ -5,7 +5,29 @@ from rest_framework.response import Response
 from django.forms.models import model_to_dict
 from .models import *
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny 
+from rest_framework.permissions import AllowAny import math
+=======
+
+from .serializer import AddressSerializer, PropertyDetailsSerializer,PropertySerializer
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
+from .models import Property, Address, PropertyDetails
+
+from rest_framework.views import APIView
+
+<<<<<<< HEAD
+import math
+=======
+
+from .serializer import AddressSerializer, PropertyDetailsSerializer,PropertySerializer
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
+from .models import Property, Address, PropertyDetails
+
+from rest_framework.views import APIView
+>>>>>>> 79bfe526891b114ccd47ce69edfe4de5a8230aa2
 # Create your views here.
 
 
