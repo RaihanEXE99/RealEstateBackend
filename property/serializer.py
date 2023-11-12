978358fde5b24d8a41e0dcb967e9ab2140d69126
RaihanEXE99 <brainsta@brainstation.com.bd>
2023-11-12 14:51:13 +0600

    Serializer Update on property

--- property/serializer.py
+++ property/serializer.py
@@ -1,3 +1,4 @@
+import json
 from rest_framework import serializers
 from .models import Property, Address, PropertyDetails
 
@@ -11,20 +12,67 @@ class PropertyDetailsSerializer(serializers.ModelSerializer):
         model = PropertyDetails
         fields = '__all__'
 
+class NoValidationField(serializers.CharField):  # Example: Use CharField for 'address'
+    def to_internal_value(self, data):
+        return data 
+    
 class PropertySerializer(serializers.ModelSerializer):
-    address = AddressSerializer()
-    details = PropertyDetailsSerializer()
-
+    address = NoValidationField()
+    details = NoValidationField()
     class Meta:
         model = Property
         fields = '__all__'
-
     def create(self, validated_data):
         address_data = validated_data.pop('address')
         details_data = validated_data.pop('details')
 
         address = Address.objects.create(**address_data)
         details = PropertyDetails.objects.create(**details_data)
-
-        property = Property.objects.create(address=address, details=details, **validated_data)
+        property = Property.objects.create(address=address,details=details, **validated_data)
         return property
+    
+    #  address_obj, created = Address.objects.create(
+    #             house=address_data['house'],
+    #             street=address_data['street'],
+    #             city=address_data['city'],
+    #             state=address_data['state'],
+    #             country=address_data['country'],
+    #             zip=address_data['zip']
+    #         )
+    #     details_obj, created = PropertyDetails.objects.create(
+    #             cid=details_data['cid'],
+    #             size_unit=details_data['size_unit'],
+    #             size=details_data['size'],
+    #             rooms=details_data['rooms'],
+    #             bed=details_data['bed'],
+    #             bath=details_data['bath'],
+    #             floor=details_data['floor'],
+    #             built=details_data['built'],
+    #             structure=details_data['structure'],
+    #             garages=details_data['garages'],
+    #             garage_size=details_data['garage_size'],
+    #         )
+# class PropertySerializer(serializers.ModelSerializer):
+#     # address = AddressSerializer()
+#     details = PropertyDetailsSerializer()
+
+#     class Meta:
+#         model = Property
+#         fields = '__all__'
+
+#     def create(self, validated_data):
+#         address_data = validated_data.pop('address')
+#         details_data = validated_data.pop('details')
+#         address_obj = Address.objects.create(
+#                 house=address_data['house'],
+#                 street=address_data['street'],
+#                 city=address_data['city'],
+#                 state=address_data['state'],
+#                 country=address_data['country'],
+#                 zip=address_data['zip']
+#             )
+#         details = PropertyDetails.objects.create(**details_data)
+
+#         # address = Address.objects.create(address_obj.pk)
+#         property = Property.objects.create(address=address_obj, details=details, **validated_data)
+#         return property
--- property/views.py
+++ property/views.py
@@ -80,11 +80,96 @@ def property(request, sku):
 
 class PropertyCreateView(APIView):
     def post(self, request, format=None):
+        request.data['address']=json.loads(request.data['address'])
+        request.data['details']=json.loads(request.data['details'])
         serializer = PropertySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # def post(self, request, format=None):
+    #     # Extract data from the request
+    #     property_data = request.data
+    #     address_data = property_data.pop('address', None)
+    #     details_data = property_data.pop('details', None)
+
+    #     # Create Address instance
+    #     address_instance = Address.objects.create(**address_data) if address_data else None
+
+    #     # Create PropertyDetails instance
+    #     details_instance = PropertyDetails.objects.create(**details_data) if details_data else None
+
+    #     # Create Property instance
+    #     property_instance = Property.objects.create(address=address_instance, details=details_instance, **property_data)
+
+    #     return Response({"sku": str(property_instance.sku)}, status=status.HTTP_201_CREATED)
+    # def post(self, request, format=None):
+    #     print("hellooooooo"*100)
+    #     # Extract data from the request
+    #     property_data = request.data
+    #     request.data['address'] = json.loads(request.data['address'])
+    #     request.data['detail'] = json.loads(request.data['detail'])
+    #     address_data = property_data.get('address', None)
+    #     details_data = property_data.pop('detail', None)
+
+    #     # Create Address instance if data is present
+    #     print(property_data)
+    #     print(address_data)
+    #     # print(request.POST['address[house]'])
+    #     if address_data:
+    #         address_instance = Address.objects.create(
+    #             house=address_data.get('house'),
+    #             street=address_data.get('street'),
+    #             city=address_data.get('city'),
+    #             state=address_data.get('state'),
+    #             country=address_data.get('country'),
+    #             zip=address_data.get('zip')
+    #         )
+    #     else:
+    #         address_instance = None
+
+    #     # Create PropertyDetails instance if data is present
+    #     if details_data:
+    #         details_instance = PropertyDetails.objects.create(
+    #             cid=details_data.get('cid'),
+    #             size_unit=details_data.get('size_unit'),
+    #             size=details_data.get('size'),
+    #             rooms=details_data.get('rooms'),
+    #             bed=details_data.get('bed'),
+    #             bath=details_data.get('bath'),
+    #             floor=details_data.get('floor'),
+    #             built=details_data.get('built'),
+    #             structure=details_data.get('structure'),
+    #             garage=details_data.get('garage'),
+    #             garage_size=details_data.get('garage_size'),
+    #             available_from=details_data.get('available_from')
+    #         )
+    #     else:
+    #         details_instance = None
+
+    #     # Create Property instance
+    #     property_instance = Property.objects.create(
+    #         sku=property_data.get('sku'),
+    #         user_id=property_data.get('user'),  # Assuming user_id is provided in request data
+    #         title=property_data.get('title'),
+    #         price=property_data.get('price'),
+    #         price_unit=property_data.get('price_unit'),
+    #         price_type=property_data.get('price_type'),
+    #         thumbnail=property_data.get('thumbnail'),
+    #         property_category=property_data.get('property_category'),
+    #         property_status=property_data.get('property_status'),
+    #         post_type=property_data.get('post_type'),
+    #         loc=property_data.get('loc'),
+    #         lat=property_data.get('lat'),
+    #         long=property_data.get('long'),
+    #         desc=property_data.get('desc'),
+    #         address=address_instance,
+    #         details=details_instance,
+    #         hide_contact=property_data.get('hide_contact')
+    #     )
+    #     return Response({"status":"Done!"},status=status.HTTP_201_CREATED)
+    #     return Response({"sku": str(property_instance.sku)}, status=status.HTTP_201_CREATED)
     
 # @api_view(['GET'])
 # def property(request, *args, **kwargs):
