6df0fad69ea01f93a67c9a1b220bc0b1802d9e76
Sadman Sakib Jisan <65308095+JisanEAE77@users.noreply.github.com>
2023-11-16 03:13:16 +0600

    Update serializer.py

--- property/serializer.py
+++ property/serializer.py
@@ -143,7 +143,7 @@ class PropertySerializerAll(serializers.ModelSerializer):
     address = AddressSerializer()
     details = PropertyDetailsSerializer()
     images = ImageSerializer(many=True)
-    video = VideoSerializer()
+    video = VideoSerializer(many=True)
 
     class Meta:
         model = Property
